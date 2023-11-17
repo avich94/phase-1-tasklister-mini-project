c9d77bb8c8fdd74d9409c96c87a54a9d8ac23406
Austin Vichich <avichich4@gmail.com>
2023-11-17 08:22:11 -0700

    lab completed/will come back for bonus Q's

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const inputElement = document.getElementById("new-task-description");
+    const task = inputElement.value;
+    const colorInput = document.getElementById("priorityColors");
+    const selectedColor = colorInput.value;
+    buildToDo(task, selectedColor);
+    form.reset();
+  });
 });
+
+function buildToDo(todo, color) {
+  let p = document.createElement('p');
+  let submit = document.createElement('button');
+  submit.addEventListener('click', handleDelete);
+  submit.textContent = 'x';
+  p.textContent = `${todo} `;
+  p.appendChild(submit);
+  p.style.color = color;
+  console.log(p);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove();
+}
